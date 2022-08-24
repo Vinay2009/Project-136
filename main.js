54553c27c816cfa762c5f22cc9885066decab517
Vinay2009 <91785104+Vinay2009@users.noreply.github.com>
2022-08-24 19:32:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
-video="";
 status = "";
-
-function preload() {
-    video = createVideo('video.mp4');
-    video.hide();
-}
+objects=[];
 
 function setup() {
     canvas = createCanvas(500, 350);
     canvas.position(500, 150);
+    video = createCapture(VIDEO);
+    video.hide();
 }
 
 function draw() {
     image(video, 0, 0, 500, 350);
+    if(status !="")
+    {
+        objectDetector.detect(video, gotResult);
+        for(i=0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects detected are:" + objects.length;
+            
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 15 , objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function start() {
@@ -23,7 +35,13 @@ function start() {
 function modelLoaded() {
     console.log("Model Loaded!");
     status = true;
-    video.loop();
-    video.speed(1);
-    video.volume(0);
+}
+
+function gotResult(error, results) {
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
